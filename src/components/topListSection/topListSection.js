83ae9ecce4af615f38a4b861e27e3ecbbbcb0cce
Bachir <bachirsalek0598@gmail.com>
2022-03-28 18:56:53 +0200

    tabla

--- src/components/topListSection/topListSection.js
+++ src/components/topListSection/topListSection.js
@@ -1,4 +1,3 @@
-import "./topListSection.css";
 import React, { useEffect, useState } from "react";
 import TopCard from "../topCard/topCard";
 import "./topListSection.css";
@@ -26,31 +25,34 @@ const TopListSection = () => {
   return (
     <section className="topList">
       <h2>Top List</h2>
-
-      {/* extra  */}
-      <div className="heading">
-        <p className="coin-name">Img</p>
-        <p className="coin-name">FullName</p>
-        <p className="coin-name">Name</p>
-        <p className="coin-name">Id</p>
-        <p className="coin-name">24h Change</p>
-        <p className="coin-price">Price</p>
-      </div>
-
-      <div className="topCoins">
-        {topCoins?.map((coin) => {
-          return (
-            <TopCard
-              key={coin.CoinInfo.Id}
-              ImageUrl={`https://www.cryptocompare.com${coin.CoinInfo.ImageUrl}`}
-              FullName={coin.CoinInfo.FullName}
-              Name={coin.CoinInfo.Name}
-              Id={coin.CoinInfo.Id}
-              Change24h={coin.DISPLAY.USD.CHANGEPCT24HOUR}
-              Price={coin.RAW.USD.PRICE}
-            />
-          );
-        })}
+      <div className="coinsItem">
+        <table>
+          <thead>
+            <tr>
+              <th>Img</th>
+              <th>FullName</th>
+              <th>Name</th>
+              <th>Id</th>
+              <th>24h Change</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody className="topCoins">
+            {topCoins?.map((coin) => {
+              return (
+                <TopCard
+                  key={coin.CoinInfo.Id}
+                  ImageUrl={`https://www.cryptocompare.com${coin.CoinInfo.ImageUrl}`}
+                  FullName={coin.CoinInfo.FullName}
+                  Name={coin.CoinInfo.Name}
+                  Id={coin.CoinInfo.Id}
+                  Change24h={coin.DISPLAY.USD.CHANGEPCT24HOUR}
+                  Price={coin.RAW.USD.PRICE}
+                />
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </section>
   );
